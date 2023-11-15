d0031693c36512d91ed8bfe84a0c7c0af4b183d8
Ed Simmons <e.d.simmons@outlook.com>
2023-11-15 16:56:35 +0000

    WiP: functionality

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ class App extends Component {
     return (
       <div id="drum-machine" className="app">
         <Display />
-
-        <body>
-          <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
-        </body>
       </div>
     );
   }
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -3,11 +3,37 @@ import './App.css';
 
 
 class DrumPad extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.onClickPlay = this.onClickPlay.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyDown);
+    };
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown(e) {
+        if (e.repeat || e.key.toLowerCase() !== this.props.id.toLowerCase()) return;
+
+        this.onClickPlay();
+    }
+
+    onClickPlay() {
+        new Audio(this.props.src).play();
+    }
+
     render() {
         return (
-            <div id={this.props.id} className="drum-pad-container">
-                <div className="drum-pad">
+            <div className="drum-pad-container">
+                <div id={this.props.name} className="drum-pad" onClick={this.onClickPlay.bind(this)}>
                     {this.props.id}
+                    <audio id={this.props.id} src={this.props.src} className="clip" />
                 </div>
             </div>
         );
